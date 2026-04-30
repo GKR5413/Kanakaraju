e0c79aea3ee6b790b72029d7d2bc3db0163387a8
GKR5413 <gkr5413@gmail.com>
2026-04-30 11:29:13 -0500

    Fix hydration mismatch: add suppressHydrationWarning to html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${bricolage.variable} ${fraunces.variable} ${inter.variable} ${jetbrainsMono.variable}`}
     >
       <head>
